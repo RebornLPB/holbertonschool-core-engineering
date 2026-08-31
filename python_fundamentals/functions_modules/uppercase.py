eb74685fef3064e5e9600bde2e4813a035802f2c
Dylan <reborndiscord@gmx.fr>
2026-08-31 15:01:20 +0200

    Refactor uppercase function to accumulate results before printing

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 def uppercase(str):
+	result = ""
 	for letter in str:
 		code = ord(str)
 		calc = code - 32
-		result = chr(calc)
-		print(f"{result}", end="")
+		result += chr(calc)
+
+	print(f"{result}", end="")
