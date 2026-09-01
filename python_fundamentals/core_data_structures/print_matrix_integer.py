00f3c5c21a3d73bab6d8aed56c0e537ce51bb970
Dylan <reborndiscord@gmx.fr>
2026-09-01 15:42:01 +0200

    Add print_matrix_integer function to print elements of a matrix

---
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def print_matrix_integer(matrix=[[]]):
+    for lists in matrix:
+        for number in range(len(lists)):
+            if (number < len(lists) - 1):
+                print("{:d}".format(number),  end=" ")
+            else:
+                print("{:d}".format(number))
