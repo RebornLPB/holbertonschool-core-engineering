068821edc9e92e50dc9716f022cbf15908926eba
Dylan <reborndiscord@gmx.fr>
2026-09-07 15:37:11 +0200

    Refactor Square class constructor to set default size and simplify type check

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -8,10 +8,9 @@ Second class made so far
 
 class Square:
     """Second class made so far"""
-    def __init__(self, size):
-        if isinstance(size, int) == False:
+    def __init__(self, size=0):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
