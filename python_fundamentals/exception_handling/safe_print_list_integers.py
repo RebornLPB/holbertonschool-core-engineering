ecead0a292bdac95b36d8b674e8912a04823625a
Dylan <reborndiscord@gmx.fr>
2026-09-07 11:22:30 +0200

    Add safe_print_list_integers function to safely print integers from a list

---
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (IndexError, TypeError, ValueError):
+            pass
+    print()
+    return count
