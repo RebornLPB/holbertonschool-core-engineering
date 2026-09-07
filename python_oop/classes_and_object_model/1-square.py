ac24fa6fe7ffc229d377930907f2ee8fba8ce5c3
Dylan <reborndiscord@gmx.fr>
2026-09-07 15:27:55 +0200

    Add initial implementation of Square class in 1-square.py

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Module 1-square
+Second class made so far
+"""
+
+
+class Square:
+    """Second class made so far"""
+    def __init__(self, size):
+        self.__size = size
