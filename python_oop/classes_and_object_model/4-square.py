833162d489e48906cd586a0d408322cdca0ef432
Dylan <reborndiscord@gmx.fr>
2026-09-07 15:49:41 +0200

    Add implementation of Square class with size validation in 4-square.py

---
+++ python_oop/classes_and_object_model/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+Module 1-square
+Fifth class made so far
+"""
+
+
+class Square:
+    """Fifth class made so far"""
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
