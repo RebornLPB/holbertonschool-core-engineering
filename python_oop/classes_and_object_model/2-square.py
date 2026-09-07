6289e5e897372f6f2faf6dbb7902210c5d35e097
Dylan <reborndiscord@gmx.fr>
2026-09-07 15:42:21 +0200

    Update class descriptions in 2-square.py and 3-square.py for consistency

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -2,12 +2,12 @@
 
 """
 Module 1-square
-Second class made so far
+Third class made so far
 """
 
 
 class Square:
-    """Second class made so far"""
+    """Third class made so far"""
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
---
+++ python_oop/classes_and_object_model/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Module 1-square
+Fourth class made so far
+"""
+
+
+class Square:
+    """Fourth class made so far"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
