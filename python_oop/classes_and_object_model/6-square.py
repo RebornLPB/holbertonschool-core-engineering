8fa164d9c6fe92a7a1c7a94e4f076fc5ea455a4a
Dylan <reborndiscord@gmx.fr>
2026-09-07 16:11:04 +0200

    Fix indentation in area method of Square class

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -38,7 +38,7 @@ class Square:
         self.__position = value
 
     def area(self):
-            return self.__size ** 2
+        return self.__size ** 2
 
     def my_print(self):
         print(self.__str__())
