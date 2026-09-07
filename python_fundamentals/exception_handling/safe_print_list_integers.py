ee0008c9ad19bce15b28dd2af94b5ebf7090b2af
Dylan <reborndiscord@gmx.fr>
2026-09-07 11:24:23 +0200

    Fix loop iteration in safe_print_list_integers and refine exception handling

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -2,11 +2,13 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for i in x:
+    for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (IndexError, TypeError, ValueError):
+        except (TypeError, ValueError):
             pass
+        except IndexError:
+            break
     print()
     return count
