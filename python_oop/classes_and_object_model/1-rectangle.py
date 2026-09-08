b6773fe52ef3fad74bc1fba922aa415a4d2a6789
Dylan <reborndiscord@gmx.fr>
2026-09-08 09:41:27 +0200

    Refactor __init__ method to use property setters for width and height in Rectangle class

--- python_oop/classes_and_object_model/1-rectangle.py
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -8,13 +8,13 @@ Reinforce object modeling with a second class.
 class Rectangle:
     """Rectangle shape"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
 
     @property
     def width(self):
-        self.__width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -26,7 +26,7 @@ class Rectangle:
 
     @property
     def height(self):
-        self.__height
+        return self.__height
 
     @height.setter
     def height(self, value):
