fc531c3215dc32c902f735150e88fa95d50875ed
Dylan <reborndiscord@gmx.fr>
2026-09-08 09:38:03 +0200

    Implement Rectangle class with width and height properties, including validation

---
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+"""
+Module 1-rectangle
+Reinforce object modeling with a second class.
+"""
+
+class Rectangle:
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+
+    @property
+    def width(self):
+        self.__width
+
+    @property.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        self.__height
+
+    @property.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+
+
+
