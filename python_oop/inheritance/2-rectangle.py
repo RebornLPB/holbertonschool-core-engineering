1ef93de2330410f41f0a47b6881fb70ce2a31c92
Dylan <reborndiscord@gmx.fr>
2026-09-09 10:03:57 +0200

    Add Rectangle class implementation inheriting from BaseGeometry

---
+++ python_oop/inheritance/2-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""Module 2-rectangle
+Creation of Rectangle class from the base_geometry module
+"""
+
+
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """Rectangle class from BaseGeometry"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
