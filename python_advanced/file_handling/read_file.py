29bb296076c29776215be2927af4744ead8f1d78
Dylan <reborndiscord@gmx.fr>
2026-09-21 10:10:25 +0200

    Add docstring to read_file function for clarity

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -3,5 +3,6 @@
 Reading file in python for the first time"""
 
 def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
