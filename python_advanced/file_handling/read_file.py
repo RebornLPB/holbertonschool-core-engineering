9d5970b5edecdd063bd862ef5ffa0e0969c3ff01
Dylan <reborndiscord@gmx.fr>
2026-09-21 10:12:51 +0200

    Fix formatting by adding a blank line after module docstring in read_file.py

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -2,6 +2,7 @@
 """Module read_file
 Reading file in python for the first time"""
 
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
