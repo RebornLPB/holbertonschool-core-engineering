8922cba8742d7be90888706b0a25752df4361c64
Dylan <reborndiscord@gmx.fr>
2026-09-21 10:20:29 +0200

    Refactor write_file function to return the number of characters written

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -6,4 +6,4 @@ Reading and writing a file in python for the first time"""
 def write_file(filename="", text=""):
     """Write a file."""
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(f"{text}\n")
+        return f.write(text)
